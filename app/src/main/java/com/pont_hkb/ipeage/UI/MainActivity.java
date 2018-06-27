a37253d9a0015c5f145e277d6bd7efbca3d8296c
Wilfried KOFFI <glamolondon@gmail.com>
2018-06-27 18:12:59 +0000

    Création du projet

--- app/src/main/java/com/pont_hkb/ipeage/UI/MainActivity.java
+++ app/src/main/java/com/pont_hkb/ipeage/UI/MainActivity.java
@@ -1,7 +1,9 @@
-package com.pont_hkb.ipeage;
+package com.pont_hkb.ipeage.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.pont_hkb.ipeage.R;
 
 public class MainActivity extends AppCompatActivity {
 
